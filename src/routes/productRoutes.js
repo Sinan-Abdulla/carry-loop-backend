0d7b04a6966d9fb0f2a56f33dc68fffcfa254f8a
sinan abdulla <sinanabdulla822@gmail.com>
2025-08-16 13:21:03 +0530

    a

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const Product = require('../models/Product');
-const { userAuth,admin } = require('../middleware/userAuth');
+const { userAuth, admin } = require('../middleware/userAuth');
 const User = require('../models/User');
 
 const router = express.Router();
 
 
-router.post('/createProduct',userAuth,admin, async (req, res) => {
-    
+router.post('/createProduct', userAuth, admin, async (req, res) => {
+
     try {
 
         const { name,
@@ -29,7 +29,6 @@ router.post('/createProduct',userAuth,admin, async (req, res) => {
             dimensions,
             weight,
             sku } = req.body;
-            
 
         const product = new Product({
             name,
@@ -57,12 +56,80 @@ router.post('/createProduct',userAuth,admin, async (req, res) => {
         const createdProduct = await product.save();
         res.status(201).json(createdProduct);
 
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: error.message, stack: error.stack });
     }
-    
-    
 });
+
+router.put('/:id', userAuth, admin, async (req, res) => {
+    try {
+        const { name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            sizes,
+            colors,
+            collections,
+            material,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            tags,
+            dimensions,
+            weight,
+            sku } = req.body;
+
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            product.name = name || product.name;
+            product.description = description || product.description;
+            product.price = price || product.price;
+            product.discountPrice = discountPrice || product.discountPrice;
+            product.countInStock = countInStock || product.countInStock;
+            product.category = category || product.category;
+            product.brand = brand || product.brand;
+            product.sizes = sizes || product.sizes;
+            product.colors = colors || product.colors;
+            product.collections = collections || product.collections;
+            product.material = material || product.material;
+            product.gender = gender || product.gender;
+            product.images = images || product.images;
+            product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured;
+            product.isPublished = isPublished !== undefined ? isPublished : product.isPublished;
+            product.tags = tags || product.tags;
+            product.dimensions = dimensions || product.dimensions;
+            product.weight = weight || product.weight;
+            product.sku = sku || product.sku;
+
+            const updatedproduct = await product.save();
+            res.json(updatedproduct);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch {
+        res.status(500).json({ message: 'Error updating product' });
+    }
+});
+
+router.delete("/:id", userAuth, admin, async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            await product.deleteOne();
+            res.json({ message: 'Product deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+);
 
 module.exports = router;
